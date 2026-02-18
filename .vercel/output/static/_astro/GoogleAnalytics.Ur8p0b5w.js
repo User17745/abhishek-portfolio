6cf0abb68ead7adcb62a692e16fe1fdadf957d08
User17745 <12226718+User17745@users.noreply.github.com>
2026-02-19 01:14:09 +0530

    security: Remove .vercel/output from git to prevent API key leakage
    - .vercel/output is already in .gitignore
    - Remove cached build files that may contain sensitive data
    - API keys will no longer be exposed in build artifacts

--- .vercel/output/static/_astro/GoogleAnalytics.Ur8p0b5w.js
+++
@@ -1,8 +0,0 @@
-import{j as t}from"./jsx-runtime.D_zvdyIk.js";function r({measurementId:a}){return a?t.jsxs(t.Fragment,{children:[t.jsx("script",{async:!0,src:`https://www.googletagmanager.com/gtag/js?id=${a}`}),t.jsx("script",{dangerouslySetInnerHTML:{__html:`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${a}', {
-              page_path: window.location.pathname,
-            });
-          `}})]}):null}export{r as default};
